03ef00262dd108e56396ba92991ff54446a70165
Galileu Rodrigues de Lima <54282757+rlgalileu@users.noreply.github.com>
2022-03-22 23:12:02 -0300

    save and retrieve bookmark data in local storage

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
-const nameValue = websiteNameEl.value;
+let bookmarks = [];
+let nameValue = websiteNameEl.value;
 let urlValue = websiteUrlEl.value;
 
 // Show Modal
@@ -15,9 +16,25 @@ function showModal() {
   websiteNameEl.focus();
 }
 
+// Fetch Bookmarks
+function fetchBookmarks() {
+  if (localStorage.getItem('bookmarks')) {
+    bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  } else {
+    bookmarks = [{
+        name: 'Quote Generator',
+        url: 'https://app.netlify.com/sites/galileu-quote-generator',
+      },
+    ];
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
+}
+
 // Handle Data from Form
 function storeBookmark(e) {
   e.preventDefault();
+  nameValue = websiteNameEl.value;
+  urlValue = websiteUrlEl.value;
 
   if (!urlValue.includes('http://') && !urlValue.includes('https://')) {
     urlValue = `https://${urlValue}`;
@@ -26,10 +43,21 @@ function storeBookmark(e) {
   if (!validate(nameValue, urlValue)) {
     return false;
   }
+
+  const bookmark = {
+    name: nameValue,
+    url: urlValue,
+  };
+
+  bookmarks.push(bookmark);
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  fetchBookmarks();
+  boodmarkForm.reset();
+  websiteNameEl.focus();
 }
 
 // Validate Form
-function validate(formValue, urlValue) {
+function validate(nameValue, urlValue) {
   const expression = /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
   const regex = new RegExp(expression);
 
@@ -42,7 +70,6 @@ function validate(formValue, urlValue) {
     alert('Please provide a valid web address.');
     return false;
   }
-
   return true;
 }
 
@@ -53,3 +80,6 @@ window.addEventListener('click', (e) => e.target === modal ? modal.classList.rem
 
 // Event Listener
 boodmarkForm.addEventListener('submit', storeBookmark);
+
+// On Load
+fetchBookmarks();
