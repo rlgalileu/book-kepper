ec25bb3fd588b002ba47826b61ca64bd40358e2b
Galileu Rodrigues de Lima <54282757+rlgalileu@users.noreply.github.com>
2022-03-22 22:49:06 -0300

    add form validation

--- script.js
+++ script.js
@@ -3,17 +3,53 @@ const modalShowBtn = document.getElementById('show-modal');
 const modalClose = document.getElementById('close-modal');
 const boodmarkForm = document.getElementById('bookmark-form');
 const websiteNameEl = document.getElementById('website-name');
-const websiteUrlEl = document.getElementById('bookmarks-container');
+const websiteUrlEl = document.getElementById('website-url');
+const bookmarksContainer = document.getElementById('bookmarks-container');
+
+const nameValue = websiteNameEl.value;
+let urlValue = websiteUrlEl.value;
 
 // Show Modal
 function showModal() {
   modal.classList.add('show-modal');
   websiteNameEl.focus();
-  console.log(modal)
-  console.log(showModal)
+}
+
+// Handle Data from Form
+function storeBookmark(e) {
+  e.preventDefault();
+
+  if (!urlValue.includes('http://') && !urlValue.includes('https://')) {
+    urlValue = `https://${urlValue}`;
+  }
+
+  if (!validate(nameValue, urlValue)) {
+    return false;
+  }
+}
+
+// Validate Form
+function validate(formValue, urlValue) {
+  const expression = /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
+  const regex = new RegExp(expression);
+
+  if (!nameValue || !urlValue) {
+    alert('Please submit values for both fields.')
+    return false;
+  }
+
+  if (!urlValue.match(regex)) {
+    alert('Please provide a valid web address.');
+    return false;
+  }
+
+  return true;
 }
 
 // Modal Event Listeners
 modalShowBtn.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'))
 window.addEventListener('click', (e) => e.target === modal ? modal.classList.remove('show-modal') : false);
+
+// Event Listener
+boodmarkForm.addEventListener('submit', storeBookmark);
